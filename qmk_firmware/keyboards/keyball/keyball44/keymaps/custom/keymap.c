b5ec1c5ba5527b168f4c917115afa7db9ef21c2a
hiwatee <tochika.biz@gmail.com>
2025-06-18 00:10:36 +0900

    feat:add led

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/custom/keymap.c
@@ -53,29 +53,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+layer_state_t layer_state_set_user(layer_state_t state)
+{
+  // レイヤーが1または3の場合、スクロールモードが有効になる
+  keyball_set_scroll_mode(get_highest_layer(state) == 3);
 
-    // レイヤーとLEDを連動させる
-    switch (get_highest_layer(state)) {
-        case 0:
-            rgblight_sethsv(HSV_WHITE);
-            break;
-        case 1:
-            rgblight_sethsv(HSV_RED);
-            break;
-        case 2:
-            rgblight_sethsv(HSV_GREEN);
-            break; 
-        case 3:
-            rgblight_sethsv(HSV_BLUE);
-            break;
-        default:
-            rgblight_sethsv(HSV_OFF);
-    }
+  // レイヤーとLEDを連動させる
+  uint8_t layer = biton32(state);
+  switch (layer)
+  {
+  case 1:
+    rgblight_sethsv(HSV_WHITE);
+    break;
 
-    return state;
+  default:
+    rgblight_sethsv(HSV_OFF);
+  }
+
+  return state;
 }
 
 #ifdef OLED_ENABLE
